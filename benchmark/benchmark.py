4e25e11f8bea9eee57c97234944629785a1b133d
Darkhan Akhmetov <darkhanahmetov228@gmail.com>
2025-08-23 15:27:07 +0500

    chore: update benchmark file tracked by git lfs with new size and hash

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23d48a2f8973b8a39c8782ddad6df9e6e87929b49e228de15264a709f61b9dac
-size 1520
+oid sha256:a2a53232178a982a7b127df7b96b791c759a918f3f6bb585d8f5ad3f06fcd479
+size 2825
