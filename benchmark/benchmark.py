ffd13f5057c9e900b3a0a7c291a7e13df2e021ec
Darkhan Akhmetov <darkhanahmetov228@gmail.com>
2025-08-23 15:29:30 +0500

    chore: update benchmark file in git lfs with new hash and increased size

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2a53232178a982a7b127df7b96b791c759a918f3f6bb585d8f5ad3f06fcd479
-size 2825
+oid sha256:edfe15b52c8a198d1d77236ee0c7c5490c58640ab0e453b89b6bf08c9c2e06a0
+size 5601
