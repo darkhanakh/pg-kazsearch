bf69d6456540f514880a3538dd8880b04bfa2401
Darkhan Akhmetov <darkhanahmetov228@gmail.com>
2025-09-04 19:15:18 +0500

    feat(benchmark): update benchmark script to more memory efficient

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6cd5581b7651a4178d0d0d3cdc1c9544bca6edf2e61a1dc01db52cb4561e2c1
-size 8069
+oid sha256:a34361adf0989ee1710753cd55e663ea4975f20d3a17fc28d356f36cb3b55552
+size 8652
