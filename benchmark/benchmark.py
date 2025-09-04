61350354d47ffa59e33dca501ff08d4a163db19b
Darkhan Akhmetov <darkhanahmetov228@gmail.com>
2025-09-04 19:32:41 +0500

    fix(benchmark): fix logic that caused undercounting tokens

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f71c94da9be0c8d2c98965d26c2ebb0cdb9b6fee2a4b475e06a1d2ece1c186f
-size 9037
+oid sha256:76b57920ab71ecf968d071cff0735753eb09c23b3cee2a728c1f11b76f0f692e
+size 9102
